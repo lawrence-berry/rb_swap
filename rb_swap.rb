06036f4c1c702ca2ea822e311089c90b9fba1b96
Lawrence Berry <lawrence.berry@gmail.com>
2018-12-09 23:40:02 +0000

    add in-file replacement

--- rb_swap.rb
+++ rb_swap.rb
@@ -1,15 +1,27 @@
 #! /usr/local/bin/ruby
 
 require 'fileutils'
-
 fail "Needs 2 arguments (find, replace)" unless ARGV.length == 2
+
+DRY_RUN = true
 
 original=ARGV[0]
 replacement=ARGV[1]
-
-Dir["**/*" + original + "*"].each do |f|
+search_path="**/*" + original + "*"
+Dir[search_path].each do |f|
   target = f.gsub(original, replacement)
-  puts "from #{f} to #{target}"
-  # FileUtils.mv f, target, :force => true
+  puts "from #{f} to #{target}" if DRY_RUN
+  FileUtils.mv f, target, force: true unless DRY_RUN
 end
 
+all_files = Dir.glob("**/*")
+all_files.each do |file_name|
+  next if File.directory?(file_name) || File.zero?(file_name)
+  next if file_name == "rb_swap.rb" # No idea!
+  text = File.read(File.join(Dir.pwd, file_name))
+  if text.include?(original)
+    replace = text.gsub(original, replacement)
+    puts File.join(Dir.pwd, file_name) if DRY_RUN
+    File.open(file_name, "w") { |file| file.puts replace } unless DRY_RUN
+  end
+end
